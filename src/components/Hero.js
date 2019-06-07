32bb742fb1c4e218fbdf4a544ee5afc69a0febf3
Ali <ar.k@outlook.com>
2019-06-07 15:47:09 -0700

    remove Material-ui from Hero component and style with pure CSS

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,38 +1,14 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import heroBackground from '../images/heroBackground.jpg';
-
-const useStyles = makeStyles(theme => ({
-  heroImage: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    background: `#F8F7F4 url("${heroBackground}") center no-repeat`,
-    width: "100%",
-    height: "70vh",
-    marginTop: "20px",
-    backgroundSize: "cover",
-  },
-  text: {
-    
-  }
-}));
 
 const Hero = () => {
-  const classes = useStyles();
   return(
-    <div className={classes.heroImage} >
-      <Typography className={classes.text}>
-        <Box fontSize="h3.fontSize" fontWeight="500">
-          Pix
-        </Box>
-        <Box fontWeight="fontWeightRegular">
-          Beautiful, free photos.<br />
-          Gifted by the world’s most generous community of photographers.
-        </Box>
-      </Typography>
+    <div className="hero_image">
+      <div className="hero_image_overlay"></div>
+      <div className="hero_text">
+        <h1>Pix</h1>
+        <p>Beautiful, free photos</p>
+        <p>Gifted by the world’s most generous community of photographers.</p>
+      </div>
     </div>
   )
 }
